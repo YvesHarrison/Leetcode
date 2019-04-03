462947de40531d713794190923a2b1beccae8fd1
YvesHarrison <yveslee15@gmail.com>
2019-04-03 14:11:50 -0400

    543. Diameter of Binary Tree solution 2

--- Tree/543. Diameter of Binary Tree/solution.cpp
+++ Tree/543. Diameter of Binary Tree/solution.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     int ans;
     int depth(TreeNode* root){
@@ -19,6 +19,29 @@ public:
     }
     int diameterOfBinaryTree(TreeNode* root) {
         depth(root);
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
+    int depth(TreeNode* root){
+        if(!root)return 0;
+        return max(depth(root->left),depth(root->right))+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        int ans=0;
+        while(!q.empty()){
+            TreeNode* tmp = q.front();
+            q.pop();
+            if(tmp->left)q.push(tmp->left);
+            if(tmp->right)q.push(tmp->right);
+            int dep=depth(tmp->left)+depth(tmp->right);
+            if(dep>ans)ans=dep;
+        }
         return ans;
     }
 };
